232ddf3fb6359a5d0e556470b19a921e759fd6b2
jenifferac95 <jeniffer.acostao@gmail.com>
2017-05-24 20:01:49 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -11,6 +12,16 @@ export class MyApp {
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDkMUTyKTxN0qIxleTdoRAIGdzArx6-2hU",
+      authDomain: "lab5-ionic-firebase.firebaseapp.com",
+      databaseURL: "https://lab5-ionic-firebase.firebaseio.com",
+      projectId: "lab5-ionic-firebase",
+      storageBucket: "lab5-ionic-firebase.appspot.com",
+      messagingSenderId: "755928407690"
+    });
+
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +30,3 @@ export class MyApp {
     });
   }
 }
-
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GooglePlus } from '@ionic-native/google-plus';
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  userProfile: any = null;
+  zone: NgZone;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private googlePlus: GooglePlus) {
+    this.zone = new NgZone({});
+    firebase.auth().onAuthStateChanged( user => {
+      this.zone.run( () => {
+        if (user){
+          this.userProfile = user;
+        } else {
+          this.userProfile = null;
+        }
+      });
+    });
+  }
 
+  loginUser(): void {
+    this.googlePlus.login({
+      'webClientId': '755928407690-k8398gjuhkpsaid95b2k6bth6b77u91e.apps.googleusercontent.com',
+      'offline': true
+    }).then( res => {
+      firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken))
+        .then( success => {
+          console.log("Firebase success: " + JSON.stringify(success));
+          
+        })
+        .catch( error => console.log("Firebase failure: " + JSON.stringify(error)));
+      }).catch(err => console.error("Error: ", err));
   }
 
 }
